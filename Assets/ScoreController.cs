1fcc13148a7bbb82d7aa27d41187b3be389d4769
erikoichiki <aptrk.usam@gmail.com>
2018-03-09 21:48:05 +0900

    レッスン５の課題
    ピンボールを作ったファイルです。

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class ScoreController : MonoBehaviour {
+	 
+	public GameObject scoreText;
+
+	private int score = 0;
+
+
+	// Use this for initialization
+	void Start () { 
+		score = 0;
+		this.scoreText = GameObject.Find("ScoreText");
+
+	}
+
+
+	// Update is called once per frame
+    void Update () {
+		
+		this.scoreText.GetComponent<Text> ().text = score.ToString ();
+	}
+
+	void OnCollisionEnter(Collision collision ){
+		
+		if (collision.gameObject.tag== "SmallStarTag") {
+			this.score += 50;
+
+
+		}  else if (collision.gameObject.tag== "LargeStarTag") {
+			this.score += 100;
+		
+
+		}
+
+}
+}
